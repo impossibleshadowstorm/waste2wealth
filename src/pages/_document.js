2c25863480433750f11bc5c6399577333f8fee1e
impossibleshadowstorm <impossibleshadowstorm@gmail.com>
2023-10-29 11:43:43 +0530

    meta and favicon Changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/icons/logo-white.svg" />
+        <meta
+          key="description"
+          name="description"
+          content="Waste2Wealth is our passion project, dedicated to transforming waste into valuable opportunities."
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
